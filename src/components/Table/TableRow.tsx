8b4a26c226337a563b072c5eacf8edd482000708
Hackerbot1488 <gromovartem04@mail.ru>
2020-09-13 14:19:15 +0300

    changed table row

--- src/components/Table/TableRow.tsx
+++ src/components/Table/TableRow.tsx
@@ -2,18 +2,40 @@ import React from "react";
 import { TableRowProps, TableRowColumn } from "./interfaces";
 import classNames from "class-names";
 import "./tableRow.sass";
-export const TableRow: React.FC<TableRowProps> = ({ isHeader, columns }) => {
+import icon from "../../img/arrow.svg";
+export const TableRow: React.FC<TableRowProps> = ({
+	isHeader,
+	columns,
+	changeSort,
+}) => {
 	const rowClasses = classNames("table__row", { table__row_header: isHeader });
 	let columnClasses = "";
+	let iconClasses = "";
 	return (
 		<div className={rowClasses}>
 			{columns!.map((column: TableRowColumn) => {
 				columnClasses = classNames("table__column", {
 					table__column_sorted: column.sorted,
-					arrow_up: column.active === "up",
-					arrow_down: column.active === "down",
 				});
-				return <div className={columnClasses}>{column.title}</div>;
+				iconClasses = classNames(
+					"arrow",
+					{ arrow_up: column.active === "up" },
+					{ arrow_down: column.active === "down" }
+				);
+				return (
+					<div className={columnClasses} key={column.title}>
+						<span
+							onClick={() =>
+								column.sorted && changeSort!(column.title, column.index!)
+							}
+						>
+							{column.title}
+							{column.sorted && (
+								<img className={iconClasses} src={icon} alt="^"></img>
+							)}
+						</span>
+					</div>
+				);
 			})}
 		</div>
 	);
