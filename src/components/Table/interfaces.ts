54d76f6c0c5b7c920c09fb4031e0223afe2f6e48
Hackerbot1488 <gromovartem04@mail.ru>
2020-09-13 14:21:01 +0300

    add more prop interfaces

--- src/components/Table/interfaces.ts
+++ src/components/Table/interfaces.ts
@@ -1,9 +1,37 @@
+export interface Person {
+	id: number;
+	firstName: string;
+	lastName: string;
+	email: string;
+	phone: string;
+	adress?: {
+		streetAddress: string;
+		city: string;
+		state: string;
+		zip: string;
+	};
+	description?: string;
+}
 export interface TableRowColumn {
 	title: string;
 	sorted?: boolean;
-	active?: string | boolean;
+	active?: string;
+	index?: number;
 }
 export interface TableRowProps {
 	isHeader?: boolean;
+	changeSort?: (title: string, index: number) => void;
 	columns?: TableRowColumn[];
+}
+export interface TableProps {
+	people: Person[];
+}
+export interface TableHeaderProps {
+	addPerson: (person: Person) => void;
+	sortByString: (subString: string) => void;
+}
+export interface TableModalProps {
+	isOpen: boolean;
+	closeModal: () => void;
+	addPerson: (person: Person) => void;
 }
