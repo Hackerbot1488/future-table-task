3747fe8203ace7537b21666692248dbcd42d4155
Hackerbot1488 <gromovartem04@mail.ru>
2020-09-13 14:20:05 +0300

    changed styles for ,odal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Person } from "./components/Table/interfaces";
 import { Table } from "./components/Table/Table";
 
 export const App: React.FC<{}> = () => {
+	const link =
+		"http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}";
+	const [loading, setLoading] = useState(false);
+	const [people, setPeople] = useState<Person[]>([]);
+	useEffect(() => {
+		setLoading(true);
+		fetch(link)
+			.then((response) => response.json())
+			.then((data) => {
+				setPeople(data);
+			});
+		setLoading(false);
+	}, []);
+	if (loading) {
+		return <p>Loading...</p>;
+	}
 	return (
 		<div className="App">
-			<Table />
+			<Table people={people} />
 		</div>
 	);
 };
