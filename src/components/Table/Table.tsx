5e4d86646544a0d864284a52cc24d3a8103b4e4f
Hackerbot1488 <gromovartem04@mail.ru>
2020-09-14 10:20:31 +0300

    before fixed sort

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,8 +1,9 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TableRow } from "./TableRow";
 import { Person, TableProps, TableRowColumn } from "./interfaces";
 import "./table.sass";
-import { title } from "process";
+import { TableHeader } from "./TableHeader";
+import { TableFooter } from "./TableFooter";
 export const Table: React.FC<TableProps> = ({ people }) => {
 	let headerColumns: TableRowColumn[] = [
 		{ title: "id", sorted: true, active: "default", index: 0 },
@@ -11,9 +12,34 @@ export const Table: React.FC<TableProps> = ({ people }) => {
 		{ title: "email", sorted: false },
 		{ title: "phone", sorted: false },
 	];
+	let type = "";
 	const [header, setHeader] = useState(headerColumns);
+	const [initialPersons, setInitialPersons] = useState<Person[]>([]);
 	const [persons, setPersons] = useState<Person[]>([]);
+	const [personsBeforeSort, setPersonsBeforeSort] = useState([]);
 	const [sortType, setSortType] = useState({ title: "", to: "" });
+	const [page, setPage] = useState(0);
+	const [maxPage, setMaxPage] = useState(0);
+	function compareUp(a: Person, b: Person) {
+		const prop = type;
+		if (a[prop] > b[prop]) {
+			return 1;
+		}
+		if (a[prop] < b[prop]) {
+			return -1;
+		}
+		return 0;
+	}
+	function compareDown(a: Person, b: Person) {
+		const prop = type;
+		if (a[prop] > b[prop]) {
+			return -1;
+		}
+		if (a[prop] < b[prop]) {
+			return 1;
+		}
+		return 0;
+	}
 	function changeActive(newIndex, toNew, oldIndex?, toOld?) {
 		setHeader(() => {
 			let columns: TableRowColumn[] = [];
@@ -43,75 +69,100 @@ export const Table: React.FC<TableProps> = ({ people }) => {
 		if (sortType.title === "") {
 			changeActive(index, "up");
 			setSortType({ title: title, to: "up" });
+			type = title;
+			setPersons((prev: Person[]) => prev.sort(compareUp));
 		} else if (sortType.title === title) {
 			if (sortType.to === "up") {
 				changeActive(index, "down");
 				setSortType({ title: title, to: "down" });
+				type = title;
+				setPersons((prev: Person[]) => prev.sort(compareDown));
 			} else {
 				changeActive(index, "default");
 				setSortType({ title: "", to: "" });
+				type = "";
+				setPersons(initialPersons.slice());
 			}
 		} else {
 			const oldIndex = header.findIndex((el) => el.title === sortType.title);
 			changeActive(index, "up", oldIndex, "default");
 			setSortType({ title: title, to: "up" });
+			type = title;
+			setPersons((prev: Person[]) => prev.sort(compareUp));
 		}
 	}
-	const compareUp = useCallback(
-		(a: Person, b: Person) => {
-			const prop = sortType.title;
-			if (a[prop] > b[prop]) {
-				return 1;
-			}
-			if (a[prop] < b[prop]) {
-				return -1;
-			}
-			return 0;
-		},
-		[sortType.title]
-	);
-	const compareDown = useCallback(
-		(a: Person, b: Person) => {
-			const prop = sortType.title;
-			if (a[prop] > b[prop]) {
-				return -1;
-			}
-			if (a[prop] < b[prop]) {
-				return 1;
-			}
-			return 0;
-		},
-		[sortType.title]
-	);
-	useEffect(() => {
-		setPersons(people);
-	}, [people]);
-	useEffect(() => {
-		console.log(sortType);
-		if (sortType.title === "") {
-			setPersons(people);
+	function addPerson(person: Person) {
+		setInitialPersons((prev: Person[]) => [person, ...prev]);
+		setPersons([person, ...initialPersons.slice()]);
+	}
+	function sortByString(subString: string) {
+		if (subString === "") {
+			setPersons(initialPersons.slice());
+			setMaxPage(
+				Math.floor((initialPersons.length - 1) / 50) < 0
+					? 0
+					: Math.floor((initialPersons.length - 1) / 50)
+			);
 		} else {
-			if (sortType.to === "up") {
-				console.log("up");
-				persons.sort(compareUp);
-			} else if (sortType.to === "down") {
-				persons.sort(compareDown);
-			}
+			const reg = new RegExp(`${subString}`);
+			let i = 0;
+			setPersons(() =>
+				initialPersons.filter(
+					(pers: Person) =>
+						(reg.test(String(pers.id)) ||
+							reg.test(pers.firstName) ||
+							reg.test(pers.lastName) ||
+							reg.test(pers.phone) ||
+							reg.test(pers.email)) &&
+						i++
+				)
+			);
+			setMaxPage(Math.floor((i - 1) / 50) < 0 ? 0 : Math.floor((i - 1) / 50));
+			page > (Math.floor((i - 1) / 50) < 0 ? 0 : Math.floor((i - 1) / 50)) &&
+				setPage(Math.floor((i - 1) / 50) < 0 ? 0 : Math.floor((i - 1) / 50));
 		}
-	}, [sortType, people, persons, compareDown, compareUp]);
+	}
+	function leftClick(current: number) {
+		setPage(current - 1);
+	}
+	function rightClick(current: number) {
+		setPage(current + 1);
+	}
+	useEffect(() => {
+		setPersons(people.slice());
+		setInitialPersons(people.slice());
+		setMaxPage(
+			Math.floor((people.length - 1) / 50) < 0
+				? 0
+				: Math.floor((people.length - 1) / 50)
+		);
+	}, [people]);
+
 	return (
 		<div className="table">
+			<TableHeader addPerson={addPerson} sortByString={sortByString} />
 			<TableRow isHeader columns={header} changeSort={changeSort} />
-			{persons.map((pers: any, index) => {
-				const columns = [
-					{ title: pers.id },
-					{ title: pers.firstName },
-					{ title: pers.lastName },
-					{ title: pers.email },
-					{ title: pers.phone },
-				];
-				return <TableRow columns={columns} key={title + index} />;
-			})}
+			{persons
+				.slice(
+					page * 50,
+					page * 50 + 50 > persons.length ? persons.length : page * 50 + 50
+				)
+				.map((pers: any, index) => {
+					const columns = [
+						{ title: pers.id },
+						{ title: pers.firstName },
+						{ title: pers.lastName },
+						{ title: pers.email },
+						{ title: pers.phone },
+					];
+					return <TableRow columns={columns} key={Math.random() + index} />;
+				})}
+			<TableFooter
+				current={page}
+				max={maxPage}
+				onLeftClick={leftClick}
+				onRightClick={rightClick}
+			/>
 		</div>
 	);
 };
